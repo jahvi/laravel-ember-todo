a2b8423226194eb5c8354565428b88a418744cb5
Javier Villanueva <javiervd@gmail.com>
2013-05-11 22:54:43 -0430

    Add pagodabox config

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => $_SERVER['DB1_HOST'],
+			'database'  => $_SERVER['DB1_NAME'],
+			'username'  => $_SERVER['DB1_USER'],
+			'password'  => $_SERVER['DB1_PASS'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
