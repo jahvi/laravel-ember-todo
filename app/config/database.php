872b8bb34571b4fb3f825bc75b8e43d012095aae
Javier Villanueva <javiervd@gmail.com>
2013-06-12 09:14:57 -0430

    Make sure the database server variables are set in the live server

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => $_SERVER['DB1_HOST'],
-			'database'  => $_SERVER['DB1_NAME'],
-			'username'  => $_SERVER['DB1_USER'],
-			'password'  => $_SERVER['DB1_PASS'],
+			'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : '',
+			'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : '',
+			'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : '',
+			'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
