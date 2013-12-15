4c5a684231c36bc77a5b27559795de889639c633
Javier Villanueva <javiervd@gmail.com>
2013-12-15 11:48:15 -0430

    Clean up production database variables

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : '',
-			'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : '',
-			'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : '',
-			'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : '',
+			'host'      => getenv('DB1_HOST'),
+			'database'  => getenv('DB1_NAME'),
+			'username'  => getenv('DB1_USER'),
+			'password'  => getenv('DB1_PASS'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
